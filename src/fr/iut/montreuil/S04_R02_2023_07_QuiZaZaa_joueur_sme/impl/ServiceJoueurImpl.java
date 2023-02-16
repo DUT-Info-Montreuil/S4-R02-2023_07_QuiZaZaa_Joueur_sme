189b4b9e2200242567b8cbf13ccc34960d23156a
nathanfnl <nathan.fnls@gmail.com>
2023-02-16 17:44:47 +0100

    debut du dev des services ajout, suppression et listage joueur

---
+++ src/fr/iut/montreuil/S04_R02_2023_07_QuiZaZaa_joueur_sme/impl/ServiceJoueurImpl.java
@@ -0,0 +1,63 @@
+package fr.iut.montreuil.S04_R02_2023_07_QuiZaZaa_joueur_sme.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import fr.iut.montreuil.S04_R02_2023_07_QuiZaZaa_joueur_sme.entities.dto.JoueurDTO;
+import fr.iut.montreuil.S04_R02_2023_07_QuiZaZaa_joueur_sme.modeles.IServiceJoueur;
+
+public class ServiceJoueurImpl implements IServiceJoueur{
+
+	private static ServiceJoueurImpl uniqueInstance=null;
+	
+	private List<JoueurDTO> joueurs;
+	
+	private ServiceJoueurImpl() {
+		this.joueurs = new ArrayList<>();
+	}
+	
+	public static ServiceJoueurImpl getInstatnce() {
+		if(uniqueInstance==null) {
+			uniqueInstance= new ServiceJoueurImpl();
+			}
+			return uniqueInstance;
+	}
+	
+	@Override
+	public boolean ajouterJoueur(JoueurDTO jd) {
+		if (this.joueurs.contains(jd)) {
+			System.out.println("joueur existant");
+			return false;
+		}
+		else if (joueurConforme(jd)) {
+			joueurs.add(jd);
+			return true;
+		}
+		return false;
+	}
+
+	private boolean joueurConforme(JoueurDTO jd) {
+		return jd != null;
+	}
+
+	@Override
+	public boolean supprimerJoueur(String pseudo) {
+		if (this.joueurs.contains(getJoueurAvecPseudo(pseudo))){
+			this.joueurs.remove(getJoueurAvecPseudo(pseudo));
+			return true;
+		}
+		return false;
+	}
+
+	public Optional<JoueurDTO> getJoueurAvecPseudo(String pseudo) {
+		return this.joueurs.stream().filter(x->x.getPseudo().equals(pseudo)).findAny();
+	}
+
+	@Override
+	public List<JoueurDTO> listerJoueur() {
+		return this.joueurs;
+	}
+
+
+}

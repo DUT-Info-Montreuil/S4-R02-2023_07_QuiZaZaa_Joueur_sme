0be269daa19a1635bd107623e4d7d7eff38075b0
nathanfnl <nathan.fnls@gmail.com>
2023-02-16 17:07:41 +0100

    ajout de la classe joueurDTO + interface IService

---
+++ src/fr/iut/montreuil/S04_R02_2023_07_QuiZaZaa_joueur_sme/entities/dto/JoueurDTO.java
@@ -0,0 +1,67 @@
+package fr.iut.montreuil.S04_R02_2023_07_QuiZaZaa_joueur_sme.entities.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JoueurDTO {
+	
+	private String prenom, pseudo;
+	private int annee_naissance, langue_preferee;
+	private List<String> centre_interets = new ArrayList<>();
+
+	public JoueurDTO(String prenom, String pseudo, int annee_naissance, int langue_preferee,
+			List<String> centre_interets) {
+		super();
+		this.prenom = prenom;
+		this.pseudo = pseudo;
+		this.annee_naissance = annee_naissance;
+		this.langue_preferee = langue_preferee;
+		this.centre_interets = centre_interets;
+	}
+
+	@Override
+	public String toString() {
+		return "JoueurDTO [prenom=" + prenom + ", pseudo=" + pseudo + ", annee_naissance=" + annee_naissance
+				+ ", langue_preferee=" + langue_preferee + ", centre_interets=" + centre_interets + "]";
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public int getAnnee_naissance() {
+		return annee_naissance;
+	}
+
+	public void setAnnee_naissance(int annee_naissance) {
+		this.annee_naissance = annee_naissance;
+	}
+
+	public int getLangue_preferee() {
+		return langue_preferee;
+	}
+
+	public void setLangue_preferee(int langue_preferee) {
+		this.langue_preferee = langue_preferee;
+	}
+
+	public List<String> getCentre_interets() {
+		return centre_interets;
+	}
+
+	public void setCentre_interets(List<String> centre_interets) {
+		this.centre_interets = centre_interets;
+	}
+}
---
+++ src/fr/iut/montreuil/S04_R02_2023_07_QuiZaZaa_joueur_sme/impl/IServiceJoueur.java
@@ -0,0 +1,15 @@
+package fr.iut.montreuil.S04_R02_2023_07_QuiZaZaa_joueur_sme.impl;
+
+import java.util.List;
+
+import fr.iut.montreuil.S04_R02_2023_07_QuiZaZaa_joueur_sme.entities.dto.JoueurDTO;
+
+public interface IServiceJoueur {
+	
+	public boolean ajouterJoueur(JoueurDTO jd);
+	
+	public boolean supprimerJoueur(String pseudo);
+	
+	public List<JoueurDTO> listerJoueur();
+
+}
